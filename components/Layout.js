219c15cf642de1333fe3b94e45a03cc5e6827afb
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-03-26 11:05:29 +0100

    add Toaster to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,25 @@
+import { Toaster } from "react-hot-toast";
 import NavigationCard from "./NavigationCard";
 
-export default function Layout({children, hideNavigation}) {
-    let rightColumnClasses = '';
-    if (hideNavigation) {
-        rightColumnClasses += 'w-full';
-        } else {
-        rightColumnClasses += 'mx-4 md:max-0 md:w-9/12';
-    }
-    return(
-        <div className='md:flex mt-4 max-w-4xl mx-auto gap-6'>
-            <title>PostMate</title>
-            {!hideNavigation && (
-                <div className='fixed md:static w-full bottom-0 md:w-3/12 -mb-5'>
-                    <NavigationCard />
-                </div>
-            )}  
-            <div className={rightColumnClasses}>
-                {children}
-                </div>
+export default function Layout({ children, hideNavigation }) {
+  let rightColumnClasses = "";
+  if (hideNavigation) {
+    rightColumnClasses += "w-full";
+  } else {
+    rightColumnClasses += "mx-4 md:max-0 md:w-9/12";
+  }
+  return (
+    <div className="md:flex mt-4 max-w-4xl mx-auto gap-6">
+      <title>PostMate</title>
+      {!hideNavigation && (
+        <div className="fixed md:static w-full bottom-0 md:w-3/12 -mb-5">
+          <NavigationCard />
         </div>
-    )
+      )}
+      <div className={rightColumnClasses}>
+        <Toaster position="top-center" />
+        {children}
+      </div>
+    </div>
+  );
 }
