e943ce5cbdf071299ac74c4ee9f1098bcfbaa65e
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-04-07 16:03:43 +0100

    listen to notification

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,43 +1,57 @@
 import Avatar from "@/components/Avatar";
 import Card from "@/components/Card";
 import Layout from "@/components/Layout";
+import supabase, { getNotificationsFunction } from "@/lib/supabase";
+import Cookies from "js-cookie";
+import moment from "moment";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Notifications() {
-    return (
-        <Layout>
-            <h1 className='text-6xl mb-4 text-gray-400'>Notifications</h1>
-            <Card noPadding={true}>
-                <div className=''>
-                    <div className='flex gap-2 items-center border-b border-b-gray-100 p-4'>
-                        <Link href={'/profile'}>
-                            <Avatar />
-                        </Link>
-                        <div>
-                            <Link href={''} className={'font-semibold mr-1'}>John Doe</Link> liked your 
-                            <Link href={''} className={'text-socialBlue hover:underline ml-1'}>photo</Link>
-                        </div>
-                    </div>
-                    <div className='flex gap-2 items-center border-b border-b-gray-100 p-4'>
-                        <Link href={'/profile'}>
-                            <Avatar />
-                        </Link>
-                        <div>
-                            <Link href={''} className={'font-semibold mr-1'}>John Doe</Link> liked your 
-                            <Link href={''} className={'text-socialBlue hover:underline ml-1'}>photo</Link>
-                        </div>
-                    </div>
-                    <div className='flex gap-2 items-center border-b border-b-gray-100 p-4'>
-                        <Link href={'/profile'}>
-                            <Avatar />
-                        </Link>
-                        <div>
-                            <Link href={''} className={'font-semibold mr-1'}>John Doe</Link> liked your 
-                            <Link href={''} className={'text-socialBlue hover:underline ml-1'}>photo</Link>
-                        </div>
-                    </div>
-                </div>
-            </Card>
-        </Layout>
-    )
+  const [notifications, setNotifications] = useState({
+    loading: true,
+    data: [],
+  });
+  const id = Cookies.get("social-id");
+  useEffect(() => {
+    const channels = supabase
+      .channel("custom-insert-channel")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "notification" },
+        (payload) => {
+          const newNotification = payload.new;
+          if (newNotification.user === id) {
+            // Update notifications with new data
+            setNotifications((prevNotifications) => ({
+              loading: false,
+              data: [newNotification, ...prevNotifications.data],
+            }));
+          }
+        }
+      )
+      .subscribe();
+  }, []);
+  useEffect(() => {
+    async function getAllNotification() {
+      const notifications = await getNotificationsFunction();
+      setNotifications({ loading: false, data: notifications });
+    }
+    getAllNotification();
+  }, []);
+  return (
+    <Layout>
+      <h1 className="text-6xl mb-4 text-gray-400">Notifications</h1>
+      <Card noPadding={true}>
+        <div className="">
+          {notifications.data.map((notification) => (
+            <div className="border-b border-b-gray-100 p-4">
+              <div>{notification.message}</div>
+              <small>{moment(notification.created_at).fromNow()}</small>
+            </div>
+          ))}
+        </div>
+      </Card>
+    </Layout>
+  );
 }
