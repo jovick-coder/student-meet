ffae3fe363a1a95c1f39e8cadc7ed850787c2b7d
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-04-25 12:37:35 +0100

    re arrange layout

--- components/Layout.js
+++ components/Layout.js
@@ -8,10 +8,10 @@ export default function Layout({ children, hideNavigation }) {
     rightColumnClasses += "mx-4 md:max-0 md:w-9/12 ";
   }
   return (
-    <div className="md:flex mt-4 max-w-4xl mx-auto gap-6">
+    <div className="md:flex max-w-6xl mx-auto gap-6 h-[100vh]">
       <title>PostMate</title>
       {!hideNavigation && (
-        <div className="fixed md:static w-full bottom-0 md:w-3/12 -mb-5 z-10">
+        <div className="fixed md:static w-full bottom-0 md:w-3/12  z-10 mt-3">
           <NavigationCard />
         </div>
       )}
